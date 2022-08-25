8db73f70caa495bdeb26c6eb881bcb6dd38d7a91
Gone2Daly <71726742+Gone2Daly@users.noreply.github.com>
2022-08-26 01:08:51 +0200

    Update Update.bat

--- launcher/progs/Update.bat
+++ launcher/progs/Update.bat
@@ -1,7 +1,7 @@
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/Gone2Daly/AMP/archive/refs/heads/main.zip', 'main.zip')"
 powershell -Command "Invoke-WebRequest https://github.com/Gone2Daly/AMP/archive/refs/heads/main.zip -OutFile main.zip"
 powershell -Command "Get-ChildItem . -Filter main.zip | Expand-Archive -DestinationPath . -Force"
-XCOPY "%cd%\AMP-main" "%cd%" /S /Y /EXCLUDE:"Launch AMP.exe"
-RMDIR "%cd%\AMP-main" /Q /S
-DEL "%cd%\main.zip" /Q
+XCOPY /S /Y /C "%cd%\AMP-main" "%cd%"
+RMDIR /Q /S "%cd%\AMP-main"
+DEL /Q "%cd%\main.zip"
 TIMEOUT 3
